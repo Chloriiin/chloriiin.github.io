31079ecf561e66c2ba55ef3c9c4d9c50e131ae6b
Chloriiin <yeasterucha@gmail.com>
2025-10-04 16:41:08 -0400

    Configure for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  skipTrailingSlashRedirect: true,
   images: {
-    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.notion.so',
+      },
+    ],
   },
 };
 
